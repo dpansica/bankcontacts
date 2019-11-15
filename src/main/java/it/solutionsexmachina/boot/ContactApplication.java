3f70471578fe989ce46aef252f7f893cbacd2419
dpansica <diego.pansica@gmail.com>
2019-11-15 17:13:18 +0100

    delete contact

--- src/main/java/it/solutionsexmachina/boot/ContactApplication.java
+++ src/main/java/it/solutionsexmachina/boot/ContactApplication.java
@@ -12,7 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories("it.solutionsexmachina.repositories")
 @EntityScan("it.solutionsexmachina.entities")
 @ComponentScan({"it.solutionsexmachina.webcontrollers"})
-@Import({ ServicesConfig.class })
+@Import({ServicesConfig.class})
 public class ContactApplication {
 
     public static void main(String[] args) {
--- src/main/java/it/solutionsexmachina/webcontrollers/ContactController.java
+++ src/main/java/it/solutionsexmachina/webcontrollers/ContactController.java
@@ -6,28 +6,25 @@ import it.solutionsexmachina.filters.ContactFilter;
 import it.solutionsexmachina.services.ContactService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class ContactController {
 
     @Autowired
     private ContactService contactService;
 
-    @CrossOrigin(origins = "*")
-    @PostMapping("/contacts")
+    @RequestMapping(value = "/contacts", method = RequestMethod.POST)
     public List<ContactDO> getContacts(@RequestBody ContactFilter filter) {
         return contactService.searchContacts(filter);
     }
 
-    @CrossOrigin(origins = "*")
-    @PostMapping("/contacts/save")
+    @RequestMapping(value = "/contacts/save", method = RequestMethod.POST)
     public ContactDO save(@RequestBody ContactDTO dto) {
 
         ContactDO entity = new ContactDO();
@@ -37,10 +34,8 @@ public class ContactController {
         return contactService.save(entity);
     }
 
-    @CrossOrigin(origins = "*")
-    @PostMapping("/contacts/remove")
+    @RequestMapping(value = "/contacts/remove", method = RequestMethod.POST)
     public ContactDO remove(@RequestBody ContactDTO dto) {
         return contactService.delete(dto.getId());
     }
-
 }
