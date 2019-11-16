349534af5afd6698a767fc244c67ac7cc53373e9
dpansica <Caltavutur0_>
2019-11-16 10:23:55 +0100

    javacript template

--- src/main/java/it/solutionsexmachina/webcontrollers/ContactController.java
+++ src/main/java/it/solutionsexmachina/webcontrollers/ContactController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,8 +18,20 @@ public class ContactController {
     private ContactService contactService;
 
     @RequestMapping(value = "/contacts", method = RequestMethod.POST)
-    public List<ContactDO> getContacts(@RequestBody ContactFilter filter) {
-        return contactService.searchContacts(filter);
+    public List<ContactDTO> getContacts(@RequestBody ContactFilter filter) {
+
+        List<ContactDTO> results = new ArrayList<>();
+
+        List<ContactDO> contacts = contactService.searchContacts(filter);
+        for (ContactDO contact : contacts) {
+            ContactDTO dto = new ContactDTO();
+
+            BeanUtils.copyProperties(contact, dto);
+
+            results.add(dto);
+        }
+
+        return results;
     }
 
     @RequestMapping(value = "/contacts/save", method = RequestMethod.POST)
